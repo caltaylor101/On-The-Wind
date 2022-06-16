a02bf88a5a29939124ffb2a595811662376fa2c0
caltaylor <Caltaylor101@gmail.com>
2022-06-15 21:28:00 -0600

    Fixed Robin Prefab Bug

--- Assets/RobinVariables.cs
+++ Assets/RobinVariables.cs
@@ -20,7 +20,8 @@ public class RobinVariables : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("CreateRobin", robinStartTime, Random.Range(1, robinMaxTime));
+        //InvokeRepeating("CreateRobin", robinStartTime, Random.Range(1, robinMaxTime));
+        CreateRobin();
     }
 
     // Update is called once per frame
@@ -32,11 +33,11 @@ public class RobinVariables : MonoBehaviour
     
     public void CreateRobin()
     {
-        if (!robinSpawned)
-        {
+        //if (!robinSpawned)
+        //{
             robinSpawned = true;
             speed = Random.Range(speed, maxSpeed);
             Instantiate(robinPrefab, new Vector3(player.transform.position.x, player.transform.position.y + 10, player.transform.position.z + 200), Quaternion.identity);
-        }
+        //}
     }
 }
